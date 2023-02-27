f2610a11e6a37f5e525eb6164377338291722a4f
何志辉 <826006140@qq.com>
2023-02-27 17:42:44 +0800

    feat: 增加调试功能

--- src/render/components/DevTools.vue
+++ src/render/components/DevTools.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="">
+    <div v-if="!isProduction" class="">
         <el-button @click="logFileList">FileList</el-button>
+        <el-button @click="logConfig">ControlConfig</el-button>
     </div>
 </template>
 
 <script setup lang="ts">
 import useFileListStore from "../store/useFileListStore";
+import useControlConfig from "../store/useControlConfigStore";
+
+const isProduction = import.meta.env.MODE === 'production'
 
 const fileListStore = useFileListStore();
+const controlConfig = useControlConfig();
 
 const logFileList = () => console.log(fileListStore.filesList)
+const logConfig = () => console.log(controlConfig.config)
 </script>
 
 <style scoped lang="scss"></style>
